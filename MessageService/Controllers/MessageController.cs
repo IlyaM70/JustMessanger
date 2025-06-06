8b27a5f783b4fc83eae094eaac96124d815b96e5
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-06-06 15:41:44 -0700

    Initialized first send endpoint for MessageController

--- MessageService/Controllers/MessageController.cs
+++ MessageService/Controllers/MessageController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MessageService.Data;
+using MessageService.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using MessageService.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MessageService.Controllers
 {
@@ -6,5 +11,51 @@ namespace MessageService.Controllers
     [ApiController]
     public class MessageController : ControllerBase
     {
+        private readonly MessageDbContext _db;
+        private readonly IHubContext<MessagesHub> _hub;
+
+        public MessageController(MessageDbContext db, IHubContext<MessagesHub> hub)
+        {
+            _db = db;
+            _hub = hub;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> Send([FromBody] SendMessageDto dto)
+        {
+            // 1) Persist
+            Message msg = new Message()
+            {
+                SenderId = dto.SenderId,
+                RecipientId = dto.RecipientId,
+                Text = dto.Text,
+                SentAt = DateTime.UtcNow
+            };
+            _db.Messages.Add(msg);
+            await _db.SaveChangesAsync();
+
+            // 2) Push over SignalR
+            await _hub.Clients
+                      .User(dto.RecipientId)
+                      .SendAsync("ReceiveMessage", new
+                      {
+                          msg.Id,
+                          msg.SenderId,
+                          msg.Text,
+                          msg.SentAt
+                      });
+
+            return Ok();
+        }
+    }
+
+    public class SendMessageDto
+    {
+        [Required]
+        public string? SenderId { get; set; }
+        [Required]
+        public string? RecipientId { get; set; }
+        [Required]
+        public string? Text { get; set; }
     }
 }
