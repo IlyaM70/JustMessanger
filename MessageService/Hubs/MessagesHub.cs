f374fa88ff697397e0f2f205a1b207de9521a90a
Ilia Merkulov <77540282+IlyaM70@users.noreply.github.com>
2025-06-06 14:49:55 -0700

    Set up SignalR

---
+++ MessageService/Hubs/MessagesHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MessageService.Hubs
+{
+    public class MessagesHub : Hub
+    {
+
+    }
+}
--- MessageService/Program.cs
+++ MessageService/Program.cs
@@ -1,4 +1,5 @@
 using MessageService.Data;
+using MessageService.Hubs;
 using Microsoft.EntityFrameworkCore;
 using System;
 
@@ -6,12 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<MessageDbContext>(opts =>
-    opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapHub<MessagesHub>("/messagesHub");
 
 app.UseSwagger();
 app.UseSwaggerUI();
